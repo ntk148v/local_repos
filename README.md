b32031ed365187d794fa68ed072cc0f230e63df0
Kien Nguyen <ntk148v@gmail.com>
2017-02-24 18:04:15 +0700

    Update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Local Repos
 Create and Setups Local Repos
+
+## How to run.
+
+1. Clone this Github repository
+
+```
+git clone https://github.com/ntk148v/local_repos
+```
+
+2. Run manage.sh
+
+```
+chmod +x manage.sh
+sudo bash ./manage.sh
+```
+
+## Local APT Repository.
+
+- Environment: Ubuntu 16.04
+- Two types: Mirror & Generate.
+
+## Local YUM Repository.
+
+- Environment: Centos 7.0
--- manage.sh
+++ manage.sh
@@ -2,30 +2,30 @@
 
 choose_apt()
 {
-	echo "******************************************************************************************************"
-	read -e -p "Do you want setup APT/YUM repo?: " TYPE
-	echo "******************************************************************************************************"
+	read -p "## Do you want setup APT/YUM repo?: " TYPE
 	case $TYPE in
-		"APT|Apt|apt")
-			read -e -p "Setup local APT Repo. Choose your repo type (mirror/generate):  " APT_TYPE
+		APT|Apt|apt)
+			read -e -p "## Setup local APT Repo. Choose your repo type (mirror/generate):  " APT_TYPE
 			case $APT_TYPE in
-				"Mirror|MIRROR|mirror")
+				Mirror|MIRROR|mirror)
+					echo "## Setup local APT Repo mirror ##"
 					cd ./apt/mirror/
 					chmod +x local_apt_mirror_repos.sh
-					sudo ./local_apt_mirror_repos
+					sudo bash ./local_apt_mirror_repos
 					;;
-				"Generate|GENERATE|generate")
+				Generate|GENERATE|generate)
+					echo "## Setup local APT Repo generate ##"
 					cd ./apt/generate/
 					chmod +x local_owned_apt_generate_repos.sh
-					sudo ./local_owned_apt_generate_repos.sh
+					sudo bash ./local_owned_apt_generate_repos.sh
 					;;
 			esac
 			;;
-		"YUM|Yum|yum")
-			echo "Setup local YUM Repo "
+		YUM|Yum|yum)
+			echo "## Setup local YUM Repo ##"
 			cd ./yum/
 			chmod +x local_yum_repos.sh
-			sudo ./local_yum_repos.sh
+			sudo bash ./local_yum_repos.sh
 			;;
 	esac
 }
